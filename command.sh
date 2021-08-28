ea66ebe2fb50ca0b54bafe2705d57fb2ccc38db6
zsarge <zackarywsargent@gmail.com>
2021-08-28 17:37:59 -0400

    generate gif from .ppm files

--- command.sh
+++ command.sh
@@ -1,3 +1,7 @@
+#!/bin/env sh
+
 echo "run the following in the frames/* directory you want to convert to video:"
 
-echo "convert -delay 20 *ppm movie.gif"
+echo "convert -delay 20 *ppm output.gif"
+
+convert -delay 20 frames/*/*ppm frames/output.gif
