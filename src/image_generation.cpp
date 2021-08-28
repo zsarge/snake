839bc509ad5b52254e8f1fdf82ff2f1f0a618952
zsarge <zackarywsargent@gmail.com>
2021-08-28 14:52:19 -0400

    create buffer for snake

--- src/image_generation.cpp
+++ src/image_generation.cpp
@@ -4,17 +4,41 @@
 
 namespace IMAGE_GEN {
 	std::string generate_folder_name() {
-		// for image generation:                       
-		auto t = std::time(nullptr);                   
-		auto tm = *std::localtime(&t);                 
+		// for image generation:
+		auto t = std::time(nullptr);
+		auto tm = *std::localtime(&t);
 
-		std::ostringstream oss;                        
+		std::ostringstream oss;
 		oss << std::put_time(&tm, "%d-%m-%Y_%H-%M-%S");
 		return oss.str();
 	}
 
 	void create_folder(std::string folder_name) {
 		std::filesystem::create_directories(PREFIX + folder_name);
+	}
+
+	std::vector<std::vector<Pixel>> create_snake_buffer(Snake* snake) {
+		unsigned int b_width = snake->get_board_width();
+		unsigned int b_height = snake->get_board_height();
+
+		std::vector<std::vector<Pixel>> buffer(
+			snake->get_board_height(),
+			std::vector<Pixel>(snake->get_board_width())
+		);
+
+		for (unsigned int j = 0u; j < b_height; ++j) {
+	 		for (unsigned int i = 0u; i < b_width; ++i) {
+				Pixel pixel = {
+					(color) (i % 256),
+					(color) (j % 256),
+					(color) ((i * j) % 256),
+				};
+
+				buffer[j][i] = pixel;
+			}
+		}
+
+		return buffer;
 	}
 
 	void print_to_image(Snake* snake) {
@@ -29,22 +53,35 @@ namespace IMAGE_GEN {
 		filename.append(".ppm");
 
 		// create file stream
-		std::ofstream ofs(filename, std::ios_base::out | std::ios_base::binary);
+		std::ofstream ofs(
+			filename,
+			std::ios_base::out | std::ios_base::binary
+		);
 
-		ofs << "P6" << std::endl << b_width << ' ' << b_height << std::endl << "255" << std::endl;
+		ofs << "P6" << std::endl << b_width
+			<< ' ' << b_height << std::endl
+			<< "255" << std::endl;
 
-		for (unsigned int j = 0u; j < b_height; ++j) {
-	 		for (unsigned int i = 0u; i < b_width; ++i) {
-				// ofs << (char) (i % 256) << (char) (j % 256) << (char) ((i * j) % 256);       // red, green, blue
-				Pixel pixel = {
-					(color) (i % 256),
-					(color) (j % 256),
-					(color) ((i * j) % 256),
-				};
+		std::vector<std::vector<Pixel>> buffer = create_snake_buffer(snake);
 
-				write_pixel_to_ofstream(&ofs, pixel);
+		for (auto row : buffer) {
+			 for (auto pixel : row) {
+				write_pixel_to_ofstream(ofs, pixel);
 			}
 		}
+
+		// for (auto j = 0u; j < b_height; ++j) {
+			// for (auto i = 0u; i < b_width; ++i) {
+				// Pixel pixel = {
+					// (color) (i % 256),
+					// (color) (j % 256),
+					// (color) ((i * j) % 256),
+				// };
+
+				// // ofs << (char) pixel.r << (char) pixel.g << (char) pixel.b;       // red, green, blue
+				// write_pixel_to_ofstream(ofs, pixel);
+			// }
+		// }
 
 		ofs.close();
 
